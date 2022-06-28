2baac4cfcced545b3b845d79dbc25711dfdeffb8
danialcodes <danialcodes@gmail.com>
2022-06-28 14:36:12 +0600

    code1

--- hello.sol
+++ hello.sol
@@ -1,1 +1,17 @@
-pragma 
+pragma solidity ^0.4.0;
+
+contract Greeter{
+    string public yourname;
+
+    function Greeter() public{
+        yourname = "world";
+
+    }
+    function set(string name) public{
+        yourname = name;
+    }
+    function hello() constant public returns (string){
+        return yourname;
+    }
+
+}
